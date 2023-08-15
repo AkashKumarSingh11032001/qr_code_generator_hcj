82de04abe8c4a9a555ad99492af227ee3d33c1bf
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-08-15 23:02:44 +0530

    ./

--- main.js
+++ main.js
@@ -6,5 +6,7 @@ const API_URL = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
 function generateQR(){
 
     qrImg.src = API_URL + qrText.value;
+    console.log(qrImg.src)
+    imgBox.classList.add("show-img")
     
 }
