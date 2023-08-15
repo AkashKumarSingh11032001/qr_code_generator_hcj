f0181c7860d645288d1d05cf0dbbc13665c90b33
Akash Singh <akashkumarsingh11032001@gmail.com>
2023-08-15 22:52:51 +0530

    .

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+let imgBox = document.getElementById("imgBox")
+let qrImg = document.getElementById("qrImage")
+let qrText = document.getElementById("qrText")
 const API_URL = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="
 
 function generateQR(){
+
+    qrImg.src = API_URL + qrText.value;
     
 }
